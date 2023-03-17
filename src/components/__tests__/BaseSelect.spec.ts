7b4df999e68598c06f7294e06e5ec51eb1db06d4
Maryana <ilon.mars@yandex.ru>
2023-03-17 13:50:39 +0300

    add: select testing

---
+++ src/components/__tests__/BaseSelect.spec.ts
@@ -0,0 +1,63 @@
+import { mount, VueWrapper } from '@vue/test-utils';
+import { describe, expect, it } from 'vitest';
+import BaseSelect from '@/components/common/BaseSelect.vue';
+import { selectValues, inputSlot } from '@/utils/testDataMocks';
+
+let wrapper: VueWrapper;
+
+const singleOptionsCheck = {
+  props: {
+    modelValue: selectValues.modelValue,
+    'onUpdate:modelValue': (e: string[]) => wrapper.setProps({ modelValue: e }),
+    options: selectValues.options,
+  },
+};
+
+const multipleOptionsCheck = {
+  props: {
+    modelValue: selectValues.modelValue,
+    'onUpdate:modelValue': (e: string[]) => wrapper.setProps({ modelValue: e }),
+    options: selectValues.options,
+    multiple: true,
+  },
+};
+
+const slotCheck = {
+  props: {
+    modelValue: selectValues.modelValue,
+    options: selectValues.emptyOptions,
+  },
+  slots: {
+    default: inputSlot.slotValue,
+  },
+};
+
+describe('BaseSelect', () => {
+  it('renders slot', () => {
+    wrapper = mount(BaseSelect, slotCheck);
+
+    expect(wrapper.html()).toContain(inputSlot.expectedValue);
+  });
+
+  it('selects single option', async () => {
+    wrapper = mount(BaseSelect, singleOptionsCheck);
+
+    const select = wrapper.get('select');
+    await select.setValue(selectValues.options[0]);
+    expect(select.element.value).toBe(selectValues.expectedValue);
+    expect(wrapper.props('modelValue')).toContain(selectValues.expectedValue);
+    expect(wrapper.props('modelValue')).toBeTypeOf('object');
+    expect(Array.isArray([wrapper.props('modelValue')])).toBe(true);
+  });
+
+  it.only('selects multiple options', async () => {
+    wrapper = mount(BaseSelect, multipleOptionsCheck);
+
+    const select = wrapper.get('select');
+    await select.setValue([selectValues.options[0], selectValues.options[2]]);
+
+    expect(wrapper.props('modelValue')).toEqual(selectValues.expectedMultipleValue);
+    expect(wrapper.props('modelValue')).toBeTypeOf('object');
+    expect(Array.isArray([wrapper.props('modelValue')])).toBe(true);
+  });
+});
--- src/utils/testDataMocks.ts
+++ src/utils/testDataMocks.ts
@@ -1,3 +1,5 @@
+const OPTIONS = ['option 1', 'option 2', 'option 3'];
+
 export const checkboxValues = {
   booleanValue: false,
   stringArrValue: [] as string[],
@@ -8,6 +10,14 @@ export const inputValues = {
   modelValue: '',
   testValue: 'test',
   expectedValue: 'test',
+};
+
+export const selectValues = {
+  modelValue: [] as string[],
+  emptyOptions: [] as string[],
+  options: OPTIONS,
+  expectedValue: OPTIONS[0],
+  expectedMultipleValue: [OPTIONS[0], OPTIONS[2]],
 };
 
 export const inputSlot = {
