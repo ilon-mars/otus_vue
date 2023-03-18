4f7219c0719d6a886dd24a8ddbf8c665dbf9158a
Maryana <ilon.mars@yandex.ru>
2023-03-18 23:09:18 +0300

    add: burgerListPage tests

--- src/utils/testDataMocks.ts
+++ src/utils/testDataMocks.ts
@@ -1,12 +1,19 @@
+import Ingredients from '@/enums/ingredients';
 import { SearchParam } from '@/types/baseSearchParams';
 import { Burger, Restaurant } from '@/types/items';
 
 export const OPTIONS = ['option 1', 'option 2', 'option 3'];
 export const SEARCH_DATA: SearchParam[] = [{ name: 'Item 1' }, { name: 'Theme 2' }];
-export const RESTAURANTS: Restaurant[] = [{ name: 'cafe', address: 'street', menu: [] }];
+export const RESTAURANTS: Restaurant[] = [{ _id: 'r1', name: 'cafe', address: 'street', menu: [] }];
 export const BURGERS: Burger[] = [
-  { name: 'burger', image: '', ingredients: [], restaurants: [] },
-  { name: 'burger', image: 'src', ingredients: [], restaurants: [] },
+  { _id: 'b1', name: 'burger with empty fields', image: '', ingredients: [], restaurants: [] },
+  {
+    _id: 'b2',
+    name: 'burger with filled fields',
+    image: 'src',
+    ingredients: [Ingredients.BUN],
+    restaurants: [RESTAURANTS[0].name],
+  },
 ];
 
 export const formElemsSlots = {
