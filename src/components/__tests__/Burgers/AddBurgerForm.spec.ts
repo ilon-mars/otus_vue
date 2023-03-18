1428a4356663fb6ffeb12bb70b5030c6587bba1d
Maryana <ilon.mars@yandex.ru>
2023-03-18 21:26:10 +0300

    update: addBurgerForm tests

--- src/components/__tests__/Burgers/AddBurgerForm.spec.ts
+++ src/components/__tests__/Burgers/AddBurgerForm.spec.ts
@@ -1,14 +1,16 @@
 import { mount, VueWrapper } from '@vue/test-utils';
-import { beforeEach, describe, expect, it, vi } from 'vitest';
+import { beforeEach, afterEach, describe, expect, it, vi } from 'vitest';
 import { createTestingPinia } from '@pinia/testing';
 import AddBurgerForm from '@/components/Burgers/AddBurgerForm.vue';
 import { useBurgerStore } from '@/stores/burgers';
 import Ingredients from '@/enums/ingredients';
+import { useRestaurantStore } from '@/stores/restaurants';
 
 let wrapper: VueWrapper;
 
 const pinia = createTestingPinia({ createSpy: vi.fn });
-const store = useBurgerStore(pinia);
+const burgerStore = useBurgerStore(pinia);
+const restaurantStore = useRestaurantStore(pinia);
 
 const mountOptions = {
   global: {
@@ -25,9 +27,72 @@ const renderElementsCheck = {
   submitBtn: 'Добавить',
 };
 
+const formSubmitCheck = [
+  {
+    name: 'Name',
+    ingredients: [],
+    placesList: [],
+    restaurantList: [],
+    submitEmitValue: 0,
+  },
+  {
+    name: '',
+    ingredients: [],
+    placesList: [],
+    restaurantList: [],
+    submitEmitValue: 0,
+  },
+  {
+    name: 'Name',
+    ingredients: [Ingredients.BUN],
+    placesList: [],
+    restaurantList: [],
+    submitEmitValue: 1,
+  },
+  {
+    name: '',
+    ingredients: [Ingredients.BUN],
+    placesList: [],
+    restaurantList: [],
+    submitEmitValue: 0,
+  },
+  {
+    name: 'Name',
+    ingredients: [Ingredients.BUN],
+    placesList: ['1 ресторан'],
+    restaurantList: [{ name: '1 ресторан', address: '1й переулок', menu: [] }],
+    submitEmitValue: 1,
+  },
+  {
+    name: '',
+    ingredients: [Ingredients.BUN],
+    placesList: ['1 ресторан'],
+    restaurantList: [{ name: '1 ресторан', address: '1й переулок', menu: [] }],
+    submitEmitValue: 0,
+  },
+  {
+    name: 'Name',
+    ingredients: [],
+    placesList: ['1 ресторан'],
+    restaurantList: [{ name: '1 ресторан', address: '1й переулок', menu: [] }],
+    submitEmitValue: 0,
+  },
+  {
+    name: '',
+    ingredients: [],
+    placesList: ['1 ресторан'],
+    restaurantList: [{ name: '1 ресторан', address: '1й переулок', menu: [] }],
+    submitEmitValue: 0,
+  },
+];
+
 describe('AddBurgerForm', () => {
   beforeEach(() => {
     wrapper = mount(AddBurgerForm, mountOptions);
+  });
+
+  afterEach(() => {
+    restaurantStore.restaurants = [];
   });
 
   it('renders all elements', () => {
@@ -40,24 +105,40 @@ describe('AddBurgerForm', () => {
     expect(wrapper.html()).toContain(renderElementsCheck.submitBtn);
   });
 
-  it('submits form only with filled name', async () => {
-    const nameInput = wrapper.findComponent({ name: 'BaseInput' }).get('input');
+  it.each(formSubmitCheck)(
+    `form with filled
+      - name: $name,
+      - ingredients: $ingredients,
+      - places: $placesList,
+      - restaurants: $restaurantList
+      - submits: $submitEmitValue`,
+    async ({ name, ingredients, placesList, restaurantList, submitEmitValue }) => {
+      restaurantStore.restaurants = restaurantList;
+
+      const nameInput = wrapper.findComponent({ name: 'BaseInput' });
+      const checkbox = wrapper.findComponent({ name: 'BaseCheckbox' });
+      const select = wrapper.findComponent({ name: 'BaseSelect' });
+
+      await nameInput.setValue(name);
+      await checkbox.setValue(ingredients);
+      await select.setValue(placesList);
+      await wrapper.find('form').trigger('submit');
+
+      expect(Object.keys(wrapper.emitted()).length).toBe(submitEmitValue);
+    }
+  );
+
+  it("calls burgerStore's addBurger action", async () => {
+    const nameInput = wrapper.findAllComponents({ name: 'BaseInput' })[0];
+    const imageInput = wrapper.findAllComponents({ name: 'BaseInput' })[1];
 
     await nameInput.setValue('Name');
+    await imageInput.setValue('imageLink');
     await wrapper.find('form').trigger('submit');
 
-    expect(wrapper.emitted('submit')).toBeTruthy();
-  });
-
-  it('calls store addBurger action', async () => {
-    const nameInput = wrapper.findComponent({ name: 'BaseInput' }).get('input');
-
-    await nameInput.setValue('Name');
-    await wrapper.find('form').trigger('submit');
-
-    expect(store.addBurger).toHaveBeenCalledWith({
+    expect(burgerStore.addBurger).toHaveBeenCalledWith({
       name: 'Name',
-      image: '',
+      image: 'imageLink',
       ingredients: [Ingredients.BUN],
       restaurants: [],
     });
