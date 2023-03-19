f6c1ea12d4942b1bafb551dd4ef3d7c00cce47f0
Maryana <ilon.mars@yandex.ru>
2023-03-19 17:30:19 +0300

    update

--- src/components/__tests__/views/BurgerPage.spec.ts
+++ src/components/__tests__/views/BurgerPage.spec.ts
@@ -1,16 +1,19 @@
-import { flushPromises, mount, VueWrapper } from '@vue/test-utils';
+import { mount, VueWrapper } from '@vue/test-utils';
 import { beforeEach, afterEach, describe, expect, it, vi } from 'vitest';
 import { createTestingPinia } from '@pinia/testing';
 import { useRouter, useRoute } from 'vue-router';
 import router from '@/router';
 import BurgerPage from '@/views/BurgerPage.vue';
 import { useBurgerStore } from '@/stores/burgers';
-import { BURGERS } from '@/utils/testDataMocks';
-import { ResourceEnum } from '@/enums/resources';
 
 let wrapper: VueWrapper;
 
-vi.mock('vue-router');
+vi.mock('vue-router', () => ({
+  useRoute: vi.fn(),
+  useRouter: vi.fn(() => ({
+    push: () => {},
+  })),
+}));
 
 const pinia = createTestingPinia({ createSpy: vi.fn });
 const store = useBurgerStore(pinia);
@@ -18,45 +21,52 @@ const store = useBurgerStore(pinia);
 const mountOptions = {
   global: {
     plugins: [pinia, router],
+    stubs: ['router-link'],
   },
 };
 
 describe('BurgerPage', () => {
-  useRouter.mockReturnValue({
-    push: vi.fn(),
-  });
-
-  // Property 'mockReturnValue' does not exist on type '() => RouteLocationNormalizedLoaded'.ts(2339)
-  useRoute.mockReturnValue({
-    params: {
-      id: 'b1',
-    },
-  });
-
-  // Property 'mockReset' does not exist on type '(to: RouteLocationRaw) => Promise<void | NavigationFailure>'.ts(2339)
   beforeEach(() => {
     wrapper = mount(BurgerPage, mountOptions);
-    useRouter().push.mockReset();
   });
 
   afterEach(() => {
     store.burgers = [];
   });
 
+  // Property 'mockImplementationOnce' does not exist on type '() => RouteLocationNormalizedLoaded'.ts(2339)
   it("shows link to main page, when burger doesn't exist", () => {
+    useRoute.mockImplementationOnce(() => ({
+      params: {
+        id: '1',
+      },
+    }));
+    // Property 'mockImplementationOnce' does not exist on type '() => Router'.ts(2339)
+    const push = vi.fn();
+    useRouter.mockImplementationOnce(() => ({
+      push,
+    }));
     console.log(wrapper.html());
   });
 });
 
-
 /*
-[Vue warn]: A plugin must either be a function or an object with an "install" function.
-[Vue warn]: Failed to resolve component: router-link
-If this is a native custom element, make sure to exclude it from component resolution via compilerOptions.isCustomElement.
-  at <BurgerPage ref="VTU_COMPONENT" >
-  at <VTUROOT>
-[Vue warn]: Failed to resolve component: router-link
-If this is a native custom element, make sure to exclude it from component resolution via compilerOptions.isCustomElement.
-  at <BurgerPage ref="VTU_COMPONENT" >
-  at <VTUROOT>
+Error: [vitest] No "createRouter" export is defined on the "vue-router" mock. Did you forget to return it from "vi.mock"?
+If you need to partially mock a module, you can use "vi.importActual" inside:
+
+vi.mock("vue-router", async () => {
+  const actual = await vi.importActual("vue-router")
+  return {
+    ...actual,
+    // your mocked methods
+  },
+})
+
+❯ src/router/index.ts:6:16
+      4| import { useRestaurantStore } from '@/stores/restaurants';
+      5|
+      6| const router = createRouter({
+       |                ^
+      7|   history: createWebHistory(),
+      8|   routes: [
 */
