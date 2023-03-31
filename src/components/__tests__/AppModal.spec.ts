86954f88f40016f80a53035b7126f85efadec56c
Maryana <ilon.mars@yandex.ru>
2023-03-31 16:04:39 +0300

    update: app modal test

--- src/components/__tests__/AppModal.spec.ts
+++ src/components/__tests__/AppModal.spec.ts
@@ -1,27 +1,10 @@
 import { mount, VueWrapper } from '@vue/test-utils';
 import { afterEach, beforeEach, describe, expect, it } from 'vitest';
-// import { createTestingPinia } from '@pinia/testing';
 import { defineComponent, ref } from 'vue';
 import AppModal from '@/components/AppModal.vue';
-// import DefaultLayout from '@/layouts/DefaultLayout.vue';
 
 let wrapper: VueWrapper;
 let destination: HTMLDivElement;
-
-// const mountOptions = {
-//   global: {
-//     stubs: {
-//       DefaultLayout: {
-//         components: { AppModal },
-//         template: `<div><AppModal :isOpen="true" /></div>`,
-//         props: {
-//           isOpen: true,
-//         },
-//       },
-//     },
-//   },
-//   plugins: [createTestingPinia({ createSpy: vi.fn })],
-// };
 
 // вариант такого тестирования взят отсюда: https://github.com/vuejs/test-utils/issues/798#issuecomment-902335440
 beforeEach(() => {
@@ -50,9 +33,10 @@ describe('AppModal', () => {
         `,
       setup() {
         const isOpen = ref(false);
+
         return {
           isOpen,
-          close: () => (isOpen.value = false),
+          closeModal: () => (isOpen.value = false),
         };
       },
     });
@@ -67,25 +51,4 @@ describe('AppModal', () => {
 
     expect(document.body.outerHTML).toContain('<div class="modal">');
   });
-
-  // не работает по документации: https://test-utils.vuejs.org/guide/advanced/teleport.html#interacting-with-the-teleported-component
-  // it.only('closes after click', async () => {
-  //   const TestComponent = defineComponent({
-  //     components: { DefaultLayout, AppModal },
-  //     template: '<Suspense><DefaultLayout><AppModal /></DefaultLayout></Suspense>',
-  //     setup() {
-  //       const isOpen = ref(false);
-  //       return {
-  //         isOpen,
-  //         close: () => (isOpen.value = false),
-  //       };
-  //     },
-  //   });
-
-  //   const wrapper = mount(TestComponent, mountOptions);
-  //   // const wrapper = await mountSuspense(DefaultLayout, mountOptions);
-
-  //   const modal = wrapper.getComponent(AppModal);
-  //   await modal.get('button')
-  // });
 });
